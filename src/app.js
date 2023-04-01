64f752f5b1139757360e181787bf611f618ad899
darjageiser <123991558+darjageiser@users.noreply.github.com>
2023-04-01 23:48:37 +0200

    Added search-form

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "d239b564cc467756f4d883a3864bf3dc";
-let city = "Zürich";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
