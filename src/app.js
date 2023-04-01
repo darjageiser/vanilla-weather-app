d1e486c652918965621cd5f2de5096b918b31bfa
darjageiser <123991558+darjageiser@users.noreply.github.com>
2023-04-01 23:39:47 +0200

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -34,10 +34,16 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "d239b564cc467756f4d883a3864bf3dc";
-let city = "New York";
+let city = "Zürich";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
